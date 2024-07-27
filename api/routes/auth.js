61a994e36b055f4e717af495b55e0d13227731af
Nathaniel Evan <nathanielevan@zohomail.com>
2024-07-27 20:15:28 +0700

    Fix user auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -12,17 +12,32 @@ const connection = mysql.createConnection({
 });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 app.post('/auth', function(request, response) {
     let user = request.body.username;
     let pass = request.body.password;
     if (user && pass) {
-        connection.query('SELECT * FROM users WHERE userID LIKE ? AND password LIKE ?', [user, pass], function(error, results, fields) {
+        connection.query('SELECT * FROM user WHERE userID LIKE ? AND password LIKE ?', [user, pass], function(error, results, fields) {
+            console.log('User found');
             if (error) throw error;
             if (results.length > 0) {
                 request.session.loggedin = true;
-                request.session.username = user;
-                response.redirect('http://localhost:3000/data-insiden');
+                request.session.username = results[0].userID;
+                request.session.admin = results[0].admin;
+                if (request.session.admin > 0) {
+                    console.log('User is admin');
+                    response.redirect('http://localhost:3000/dashboard-admin');
+                }
+                else {
+                    response.redirect('http://localhost:3000/');
+                }
             }
             else {
                 response.send('Incorrect login credentials.');
